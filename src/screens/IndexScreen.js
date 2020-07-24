6d219e8df95fb33228acd1fe142512dda0f56d39
dpolozov <dpolozov@gmail.com>
2020-07-23 17:12:52 -0700

    automating context creation and deleting blogs

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,20 +1,25 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
-import BlogContext from '../context/BlogContext';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
+import {Context} from '../context/BlogContext';
+import { FontAwesome } from '@expo/vector-icons'; 
 
 const IndexScreen = () => {
 
-    const {data, addBlogPost} = useContext(BlogContext);
+    const {state, addBlogPost, deleteBlogPost} = useContext(Context);
 
     return (
         <View>
-            <Text>Index Screen</Text>
             <Button title='Add Post' onPress={addBlogPost}/>
             <FlatList 
-                data={data}
+                data={state}
                 keyExtractor={(blogPost) => blogPost.title}
                 renderItem={({item}) =>{
-                    return <Text>{item.title}</Text>
+                    return <View style={styles.row}>
+                        <Text style={styles.title}>{item.title} - {item.id}</Text>
+                        <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                            <FontAwesome name='trash' style={styles.icon} />    
+                        </TouchableOpacity>
+                    </View>
                 }}
             />
         </View>
@@ -22,6 +27,21 @@ const IndexScreen = () => {
 
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: 'gray'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
+});
 
 export default IndexScreen;
